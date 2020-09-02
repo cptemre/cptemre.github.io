ec59aad094bcf045efb04448559feb517c70c347
cptemre <kunduraci2@gmail.com>
2020-09-02 14:59:14 +0300

    Icon Animation to All Images. Basket Function Creating.
    All images now have buy icon animation.
    All images added to our store container.
    Basket function creation started. Not complited yet.

--- 2 - Javascript Practice Projects/9 - Filter JavaScript Project/script.js
+++ 2 - Javascript Practice Projects/9 - Filter JavaScript Project/script.js
@@ -1,8 +1,26 @@
 var basket = document.getElementById("basket");
 
-var product = document.getElementById("product1");
+var product = document.getElementsByClassName("product");
 
-var body = document.getElementById("body");
+var shopIconSpan = document.getElementsByClassName("shopIconSpan");
+
+var scale = document.getElementsByClassName("scale");
+
+var btnLast = document.getElementsByClassName("btnLast");
+
+var sherbet = document.getElementsByClassName("sherbet");
+
+var ice = document.getElementsByClassName("ice");
+
+var milky = document.getElementsByClassName("milky");
+
+var item = document.getElementById("item");
+
+var itemNumber = document.getElementById("itemNumber");
+
+var dollar = document.getElementById("dollar");
+
+// #region Basket icon animation when scroll down
 
 window.addEventListener("scroll", function () {  
     basket.style.position = "fixed";
@@ -11,15 +29,124 @@ window.addEventListener("scroll", function () {
     basket.style.width = "13%";
     basket.style.animation = "basketAnimation 1s ease-out";
 })
+// #endregion Basket icon animation when scroll down
 
-var shopIconSpan = document.getElementById("shopIconSpan");
+// #region Shop icon iteration for all images in last container
+for (let i = 0; i < scale.length; i++) {
+    scale[i].addEventListener("mouseover", function () {  
+        shopIconSpan[i].style.animation = "shopAnimation1 0.5s forwards";
+    })
+}
 
-var scale = document.getElementById("scale");
+for (let i = 0; i < scale.length; i++) {
+    scale[i].addEventListener("mouseout", function () {  
+        shopIconSpan[i].style.animation = "shopAnimation2 0.5s forwards";
+    })
+}
+// #endregion Shop icon iteration for all images in last container
 
-scale.addEventListener("mouseover", function () {  
-    shopIconSpan.style.animation = "shopAnimation1 0.5s forwards";
-})
 
-scale.addEventListener("mouseout", function () {  
-    shopIconSpan.style.animation = "shopAnimation2 0.5s forwards";
-})
+// #region Our store buttons' colors
+for (let i = 0; i < btnLast.length; i++) {
+    btnLast[i].addEventListener("click", function () {
+        switch (i) {
+            case 0:
+                btnLast[i].style.background = "white";
+                btnLast[i].style.color = "coral";
+
+                btnLast[i+1].style.background = "transparent";
+                btnLast[i+1].style.color = "black";
+
+                btnLast[i+2].style.background = "transparent";
+                btnLast[i+2].style.color = "black";
+                btnLast[i+3].style.background = "transparent";
+                btnLast[i+3].style.color = "black";
+
+                for (let i = 0; i < product.length; i++) {
+                    product[i].style.display = "grid";
+                }
+
+                break;
+            case 1:
+                btnLast[i].style.background = "white";
+                btnLast[i].style.color = "coral";
+    
+                btnLast[i-1].style.background = "transparent";
+                btnLast[i-1].style.color = "black";
+
+                btnLast[i+1].style.background = "transparent";
+                btnLast[i+1].style.color = "black";
+    
+                btnLast[i+2].style.background = "transparent";
+                btnLast[i+2].style.color = "black";
+
+                for (let i = 0; i < sherbet.length; i++) {
+                    sherbet[i].style.display = "grid";
+                    ice[i].style.display = "none";
+                    milky[i].style.display = "none";
+                }
+
+                break;
+            case 2:
+                btnLast[i].style.background = "white";
+                btnLast[i].style.color = "coral";
+        
+                btnLast[i-2].style.background = "transparent";
+                btnLast[i-2].style.color = "black";
+    
+                btnLast[i-1].style.background = "transparent";
+                btnLast[i-1].style.color = "black";
+        
+                btnLast[i+1].style.background = "transparent";
+                btnLast[i+1].style.color = "black";
+                
+                for (let i = 0; i < sherbet.length; i++) {
+                    sherbet[i].style.display = "none";
+                    ice[i].style.display = "grid";
+                    milky[i].style.display = "none";
+                }
+
+                break;
+            case 3:
+                btnLast[i].style.background = "white";
+                btnLast[i].style.color = "coral";
+            
+                btnLast[i-3].style.background = "transparent";
+                btnLast[i-3].style.color = "black";
+        
+                btnLast[i-2].style.background = "transparent";
+                btnLast[i-2].style.color = "black";
+            
+                btnLast[i-1].style.background = "transparent";
+                btnLast[i-1].style.color = "black";
+
+                for (let i = 0; i < sherbet.length; i++) {
+                    sherbet[i].style.display = "none";
+                    ice[i].style.display = "none";
+                    milky[i].style.display = "grid";
+                }
+
+                break;
+                    
+            default:
+                break;
+        }
+    })
+}
+// #endregion Our store buttons' colors
+for (let i = 0; i < shopIconSpan.length; i++) {
+    
+    
+    shopIconSpan[i].addEventListener("click", function () {
+        var name = document.getElementsByClassName("name");  
+        item.innerHTML = " " + name[i].innerHTML;
+    })
+}
+
+for (let i = 0; i < shopIconSpan.length; i++) {
+    shopIconSpan[i].addEventListener("click", function () {
+        itemNumber.innerHTML++;
+
+    })
+    
+}
