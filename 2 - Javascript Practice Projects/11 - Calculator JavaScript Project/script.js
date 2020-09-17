7e0a37bd54410738e8445ba5a692750693dec819
cptemre <kunduraci2@gmail.com>
2020-09-17 18:35:19 +0300

    All Operators Working
    Operators can calculate now.
    Only number 8 button is available for calculations.

--- 2 - Javascript Practice Projects/11 - Calculator JavaScript Project/script.js
+++ 2 - Javascript Practice Projects/11 - Calculator JavaScript Project/script.js
@@ -2,74 +2,269 @@ var input1 = document.getElementById("input1");
 var input1Value = input1.value;
 
 var buttons = document.getElementsByTagName("button");
+var numbers = document.getElementsByClassName("numbers");
 var newButtons = [];
+var newNumbers = [];
 
 for (let i = 0; i < buttons.length; i++) {
     newButtons.push(buttons[i]);
+    newNumbers.push(numbers[i]);
 }
+
+var firstNumber = 0;
+var secondNumber = 0;
+var operator;
+
 for (let i = 0; i < newButtons.length; i++) {
     var count = 0;
-
+    var timer0 = 0;
+    var timer1 = 0;
+    var timer2 = 0;
+    var timer3 = 0;
+    var timer4 = 0;
+    var timer5 = 0;
+    var timer6 = 0;
+    var timer7 = 0;
+    var timer8 = 0;
+    var timer9 = 0;
+    var timer10 = 0;
     newButtons[i].addEventListener("click", function () { 
 
         switch (i) {
             case 0:
                 if (count == 0) {
-                    input1.value += "*";
-                    count++;
+                    if (timer0 == 0) {
+                        firstNumber = Number(input1.value);
+                        console.log(firstNumber);
+                        input1.value += "*";
+                        operator = "*";
+                        count++;
+                        timer0++;
+                    }
+                    else if (timer0 > 0) {
+                        input1.value += "*";
+                        firstNumber = firstNumber * secondNumber;
+                    }
                 }
                 else {
                     count++;
                 }
+                timer8 = 0;
                 console.log(count);
                 break;
             case 1:
                 if (count == 0) {
-                    input1.value += "/";
-                    count++;
+                    if (timer1 == 0) {
+                        firstNumber = Number(input1.value);
+                        console.log(firstNumber);
+                        input1.value += "/";
+                        operator = "/";
+                        count++;
+                        
+                        timer1++;
+                    }
+                    else if (timer1 > 0) {
+                        input1.value += "/";
+                        firstNumber = firstNumber / secondNumber;
+                    }
                 }
                 else {
                     count++;
                 }
+                timer0 = 0;
+                timer8 = 0;
                 console.log(count);
                 break;
             case 2:
                 if (count == 0) {
-                    input1.value += "+";
-                    count++;
+                    if (timer2 == 0) {
+                        firstNumber = Number(input1.value);
+                        console.log(firstNumber);
+                        input1.value += "+";
+                        operator = "+";
+                        count++;
+                        timer2++;
+                    }
+                    else if (timer2 > 0) {
+                        input1.value += "+";
+                        firstNumber = firstNumber + secondNumber;
+                    }
                 }
                 else {
                     count++;
                 }
+                timer0 = 0;
+                timer1 = 0;
+                timer8 = 0;
                 console.log(count);
                 break;
             case 3:
                 if (count == 0) {
-                    input1.value += "-";
-                    count++;
+                    if (timer3 == 0) {
+                        firstNumber = Number(input1.value);
+                        console.log(firstNumber);
+                        input1.value += "-";
+                        operator = "-";
+                        count++;
+                        timer3++;
+                    }
+                    else if (timer3 > 0) {
+                        input1.value += "-";
+                        firstNumber = firstNumber - secondNumber;
+                    }
                 }
                 else {
                     count++;
                 }
+                timer0 = 0;
+                timer1 = 0;
+                timer2 = 0;
+                timer8 = 0;
                 console.log(count);
                 break;
             case 4:
                 if (count == 0) {
-                    input1.value += ".";
-                    count++;
+                    if (timer4 == 0) {
+                        firstNumber = Number(input1.value);
+                        console.log(firstNumber);
+                        input1.value += ".";
+                        operator = ".";
+                        count++;
+                        timer4++;
+                    }
+                    else if (timer4 > 0) {
+                        input1.value = "error";
+                    }
                 }
-                else {
-                    count++;
-                }
-                console.log(count);
+                timer0 = 0;
+                timer1 = 0;
+                timer2 = 0;
+                timer3 = 0;
+                timer8 = 0;
                 break;
             case 5:
                 count = 0
-                input1.value += 9;
+                input1.value += newNumbers[0].value;
                 break;
             case 6:
                 count = 0
-                input1.value += 8;
+                switch (timer8) {
+                    
+                    case 0:
+                        input1.value = "";
+                        input1.value += 8;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 1:
+                        input1.value = "";
+                        input1.value += 88;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 2:
+                        input1.value = "";
+                        input1.value += 888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 3:
+                        input1.value = "";
+                        input1.value += 8888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 4:
+                        input1.value = "";
+                        input1.value += 88888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 5:
+                        input1.value = "";
+                        input1.value += 888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 6:
+                        input1.value = "";
+                        input1.value += 8888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 7:
+                        input1.value = "";
+                        input1.value += 88888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 8:
+                        input1.value = "";
+                        input1.value += 888888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 9:
+                        input1.value = "";
+                        input1.value += 8888888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 10:
+                        input1.value = "";
+                        input1.value += 88888888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 11:
+                        input1.value = "";
+                        input1.value += 888888888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 12:
+                        input1.value = "";
+                        input1.value += 8888888888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 13:
+                        input1.value = "";
+                        input1.value += 88888888888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 14:
+                        input1.value = "";
+                        input1.value += 888888888888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    case 15:
+                        input1.value = "";
+                        input1.value += 8888888888888888;
+                        timer8++;
+                        secondNumber = Number(input1.value);
+                        break;
+                    default:
+                        break;
+                    console.log(timer8);
+                }
+                if (firstNumber == 0) {
+                    input1.value = secondNumber;
+                } else {
+                    input1.value = firstNumber + operator + secondNumber;
+                }
+                if (operator == "*") {
+                    input1.value = parseFloat(firstNumber * secondNumber);
+                } else if (operator == "/") {
+                    input1.value = firstNumber / secondNumber;
+                } else if (operator == "+") {
+                    input1.value = firstNumber + secondNumber;
+                } else if (operator == "-") {
+                    input1.value = firstNumber - secondNumber;
+                }
+                console.log(secondNumber);
                 break;
             case 7:
                 count = 0
@@ -99,29 +294,35 @@ for (let i = 0; i < newButtons.length; i++) {
             case 13:
                 count = 0
                 input1.value += 1;
+                Number(input1Value);
                 break;
             case 14:
                 count = 0
                 input1.value += 0;
                 break;
-            /* Equal case 15:
+            case 15:
                 count = 0
-                input1.value += 0;
-                break; */
+                timer8 = 0;
+                if (operator == "*") {
+                    input1.value = parseFloat(firstNumber * secondNumber);
+                } else if (operator == "/") {
+                    input1.value = firstNumber / secondNumber;
+                } else if (operator == "+") {
+                    input1.value = firstNumber + secondNumber;
+                } else if (operator == "-") {
+                    input1.value = firstNumber - secondNumber;
+                }
+                break;
             case 16:
                 count = 0
+                timer0 = 0;
+                timer8 = 0;
+                firstNumber = 0;
+                secondNumber = 0;
                 input1.value = "";
                 break;
             default:
                 break;
         }
-        function repeat() {  
-            if (input1Value.indexOf("*") == input1Value.indexOf(input1Value.length-1)) {
-                input1.value += "";
-            }
-            else {
-                input1.value += "";
-            }
-        } 
     })
 }
