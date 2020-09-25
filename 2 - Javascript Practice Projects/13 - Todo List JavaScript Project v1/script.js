103f30f44a4daf373b1453e1633b5460201153fe
cptemre <kunduraci2@gmail.com>
2020-09-25 11:50:42 +0300

    Adding New Element Functions is Created
    Now it is possible to add new items with add item button.

--- 2 - Javascript Practice Projects/13 - Todo List JavaScript Project v1/script.js
+++ 2 - Javascript Practice Projects/13 - Todo List JavaScript Project v1/script.js
@@ -1,27 +1,63 @@
 let button0 = document.getElementById("button0");
-let count = 3;
-let toDo = document.getElementsByClassName("toDo");
+let row = 3;
+let column = 2;
+let toDo0 = `<div class="toDo">
+<div class="newElements">
+    <div class="task">
+        asd
+    </div>
+    <div class="icons">
+        <div class="checkDiv">
+            <i class="far fa-check-square check"></i>
+        </div>
+        <div class="editDiv">
+            <i class="fas fa-user-edit edit"></i>
+        </div>
+        <div class="cancelDiv">
+            <i class="far fa-window-close cancel"></i>
+        </div>
+    </div>
+</div>`;
+let toDo1 = `<div class="toDo">
+<div class="newElements">
+    <div class="task">
+        asd
+    </div>
+    <div class="icons">
+        <div class="checkDiv">
+            <i class="far fa-check-square check"></i>
+        </div>
+        <div class="editDiv">
+            <i class="fas fa-user-edit edit"></i>
+        </div>
+        <div class="cancelDiv">
+            <i class="far fa-window-close cancel"></i>
+        </div>
+    </div>
+</div>`
 let container0 = document.getElementById("container0");
-document.body.style.gridTemplateRows = "0.1fr 0.1fr 0.1fr";
-document.body.style.gridTemplateAreas = `". header0Div ."
+container0.style.gridTemplateRows = "1fr 1.3fr 1fr";
+container0.style.gridTemplateAreas = `". header0Div ."
 ". add ."
 ". clearDiv ."`;
 let clearDiv = document.getElementById("clearDiv");
-let newToDo = [];
-for (let i = 0; i < 100; i++) {
-    newToDo.push(toDo[0]);
-}
-let i = 0
+
+let newCount  = 0
 $("#button0").click(function () {
-    newToDo[i].style.cssText = `grid-area: newToDo${i} !important;`
-    $("body").append(newToDo[i]);
-    newToDo[i].style.display = "grid";
-    document.body.style.gridTemplateRows += " 0.1fr";
-    document.body.style.gridTemplateAreas += `
-    ". newToDo${i} ."
+    $("#container0").append(toDo0);
+    let classes = document.getElementsByClassName("toDo");
+    for (let i = 0; i < 1; i++) {
+        classes[i].id = i
+    }
+    classes[newCount].style.display = "grid";
+    container0.style.gridTemplateRows += " 1fr";
+    container0.style.gridTemplateAreas += `
+    ". toDo${newCount} ."
     `;
-    newToDo[0].style.gridRow =  count;
-    clearDiv.style.gridRow = count+1;
-    count++;
-    i++;
+    classes[newCount].style.gridArea = `toDo${newCount}`;
+    classes[newCount].style.gridRow =  row;
+    classes[newCount].style.gridColumn =  column;
+    clearDiv.style.gridRow = row+1;
+    row++;
+    newCount++;
 });
