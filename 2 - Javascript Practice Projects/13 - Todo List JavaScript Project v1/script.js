6cee09721863a1f06d6b575d9ad5b6243e67e639
cptemre <kunduraci2@gmail.com>
2020-09-25 14:57:39 +0300

    Input Value Attached to Task InnerHTML and Check Icon Works

--- 2 - Javascript Practice Projects/13 - Todo List JavaScript Project v1/script.js
+++ 2 - Javascript Practice Projects/13 - Todo List JavaScript Project v1/script.js
@@ -18,34 +18,21 @@ let toDo0 = `<div class="toDo">
         </div>
     </div>
 </div>`;
-let toDo1 = `<div class="toDo">
-<div class="newElements">
-    <div class="task">
-        asd
-    </div>
-    <div class="icons">
-        <div class="checkDiv">
-            <i class="far fa-check-square check"></i>
-        </div>
-        <div class="editDiv">
-            <i class="fas fa-user-edit edit"></i>
-        </div>
-        <div class="cancelDiv">
-            <i class="far fa-window-close cancel"></i>
-        </div>
-    </div>
-</div>`
 let container0 = document.getElementById("container0");
 container0.style.gridTemplateRows = "1fr 1.3fr 1fr";
 container0.style.gridTemplateAreas = `". header0Div ."
 ". add ."
 ". clearDiv ."`;
 let clearDiv = document.getElementById("clearDiv");
+let newCount  = 0;
+let input = document.getElementById("input0");
+let task = document.getElementsByClassName("task");
 
-let newCount  = 0
 $("#button0").click(function () {
     $("#container0").append(toDo0);
     let classes = document.getElementsByClassName("toDo");
+    let check = document.getElementsByClassName("check");
+    let cancel = document.getElementsByClassName("cancel");    
     for (let i = 0; i < 1; i++) {
         classes[i].id = i
     }
@@ -57,7 +44,24 @@ $("#button0").click(function () {
     classes[newCount].style.gridArea = `toDo${newCount}`;
     classes[newCount].style.gridRow =  row;
     classes[newCount].style.gridColumn =  column;
+    task[newCount].innerHTML = input.value;
     clearDiv.style.gridRow = row+1;
+    for (let i = 0; i < check.length; i++) {
+        check[i].addEventListener("click", function () { 
+            if (task[i].style.cssText == `text-decoration: line-through; opacity: 0.7;`) {
+                task[i].style.cssText = `text-decoration: none; opacity: 1;`
+            } else {
+                task[i].style.cssText = `text-decoration: line-through; opacity: 0.7;`
+            } 
+            ;
+        })
+    }
+    for (let i = 0; i < cancel.length; i++) {
+        cancel[i].addEventListener("click", function () { 
+            classes[i].style.display = "none";
+            
+        })
+    }
     row++;
     newCount++;
 });
