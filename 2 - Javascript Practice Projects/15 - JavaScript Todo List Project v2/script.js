c90eb94d8b70f7582a7cd5bd235c902c280613dd
cptemre <kunduraci2@gmail.com>
2020-10-05 21:59:39 +0300

    Enter Key Function was Missing

--- 2 - Javascript Practice Projects/15 - JavaScript Todo List Project v2/script.js
+++ 2 - Javascript Practice Projects/15 - JavaScript Todo List Project v2/script.js
@@ -9,130 +9,71 @@ $(function () {
     </div>
 </div>
 `;
-let count = 0;
+    let count = 0;
 
-function addItem() {
+    function addItem() {
         if ($("#input").val().length == 0) {
             alert("Please write a valid item.")
         } else {
             count = $(".newItems").length;
-        let item = document.getElementsByClassName("item");
-        $(".items").append(newItems);
+            let item = document.getElementsByClassName("item");
+            $(".items").append(newItems);
             localStorage.setItem(item.length, $("#input").val());
-        $(`.item:eq(${count})`).html(localStorage.getItem(item.length));
+            $(`.item:eq(${count})`).html(localStorage.getItem(item.length));
         }
-}
-$(localStorage).each(function (index) {
+    }
+    $(localStorage).each(function (index) {
         $(".items").append(newItems);
-        $(`.item:eq(${index})`).html(localStorage.getItem(index+1));
+        $(`.item:eq(${index})`).html(localStorage.getItem(index + 1));
+        deleteFunc();
+    });
+
+    function deleteFunc() {
         $(".deleteIcon").on({
-            mouseover: function () {  
+            mouseover: function () {
                 $(this).parent().parent().css("opacity", 0.2);
                 $(this).parent().parent().css("background-color", "orangered");
             },
-            mouseleave: function () {  
+            mouseleave: function () {
                 $(this).parent().parent().css("opacity", 1);
                 $(this).parent().parent().css("background-color", "white");
             },
-            click: function () {  
+            click: function () {
                 console.log(localStorage.getItem(localStorage.length))
                 $(this).parent().parent().remove();
                 let x = $(this).parent().parent().children(0).html();
                 let i = localStorage.length;
                 while (i-- > 0) {
-                let key = localStorage.key(i);
-                if (localStorage.getItem(key) === x) {
-                    if (Number(key) == localStorage.length) {
-                        localStorage.setItem(key, localStorage.getItem(Number(key)+1))
-                        localStorage.removeItem(localStorage.length);
+                    let key = localStorage.key(i);
+                    if (localStorage.getItem(key) === x) {
+                        if (Number(key) == localStorage.length) {
+                            localStorage.setItem(key, localStorage.getItem(Number(key) + 1))
+                            localStorage.removeItem(localStorage.length);
+                        }
+                        while (Number(key) < localStorage.length) {
+                            localStorage.setItem(key, localStorage.getItem(Number(key) + 1))
+                            key++
+                            localStorage.removeItem(key);
+                        }
+
                     }
-                    while (Number(key) < localStorage.length) {
-                        localStorage.setItem(key, localStorage.getItem(Number(key)+1))
-                        key++
-                        localStorage.removeItem(key);
-                    }
-                    
                 }
-            }
             }
         });
-});
-function deleteItem() {
-    $(".deleteIcon").on({
-        mouseover: function () {  
-            $(this).parent().parent().css("opacity", 0.2);
-            $(this).parent().parent().css("background-color", "orangered");
-        },
-        mouseleave: function () {  
-            $(this).parent().parent().css("opacity", 1);
-            $(this).parent().parent().css("background-color", "white");
-        },
-        click: function () {  
-            console.log(localStorage.getItem(localStorage.length))
-            $(this).parent().parent().remove();
-            let x = $(this).parent().parent().children(0).html();
-            let i = localStorage.length;
-            while (i-- > 0) {
-            let key = localStorage.key(i);
-            if (localStorage.getItem(key) === x) {
-                if (Number(key) == localStorage.length) {
-                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
-                    localStorage.removeItem(localStorage.length);
-                }
-                while (Number(key) < localStorage.length) {
-                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
-                    key++
-                    localStorage.removeItem(key);
-                }
-                
-            }
-        }
+    };
+
+    $("#button2").click(function () {
+        localStorage.clear();
+        $(".newItems").remove();
+    });
+    $("body").keyup(function (e) {
+        if (e.keyCode === 13) {
+            addItem();
+            deleteFunc();
         }
     });
-};
-$("#button2").click(function () { 
-    localStorage.clear();
-    $(".newItems").remove();
-});
-$("body").keyup(function (e) { 
-    if (e.keyCode === 13) {
+    $("#button1").click(function () {
         addItem();
-    }
-});
-$("#button1").click(function () { 
-        addItem();
-        (function deleteItem() {
-    $(".deleteIcon").on({
-        mouseover: function () {  
-            $(this).parent().parent().css("opacity", 0.2);
-            $(this).parent().parent().css("background-color", "orangered");
-        },
-        mouseleave: function () {  
-            $(this).parent().parent().css("opacity", 1);
-            $(this).parent().parent().css("background-color", "white");
-        },
-        click: function () {  
-            console.log(localStorage.getItem(localStorage.length))
-            $(this).parent().parent().remove();
-            let x = $(this).parent().parent().children(0).html();
-            let i = localStorage.length;
-            while (i-- > 0) {
-            let key = localStorage.key(i);
-            if (localStorage.getItem(key) === x) {
-                if (Number(key) == localStorage.length) {
-                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
-                    localStorage.removeItem(localStorage.length);
-                }
-                while (Number(key) < localStorage.length) {
-                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
-                    key++
-                    localStorage.removeItem(key);
-                }
-                
-            }
-        }
-        }
-    });
-})();   
+        deleteFunc();
     });
 });
