bb431584ab1ff369498e6d378b7245c45b857ca8
cptemre <kunduraci2@gmail.com>
2020-10-05 18:15:10 +0300

    Final Version of Function If Any Error won't Happen

--- 2 - Javascript Practice Projects/15 - JavaScript Todo List Project v2/script.js
+++ 2 - Javascript Practice Projects/15 - JavaScript Todo List Project v2/script.js
@@ -10,38 +10,50 @@ $(function () {
 </div>
 `;
 let count = 0;
-for (let i = 0; i < 20; i++) {
-    if (localStorage.getItem(i)) {
-        $(".items").append(newItems);
-        $(`.item:eq(${localStorage.length-1})`).html(localStorage.getItem(i));
-        console.log(localStorage.getItem(newItems.length));
-        console.log(localStorage.length);
-        console.log(localStorage.getItem(i));
-
-    }     
+function deleteItem() {
+    $(".deleteIcon").on({
+        mouseover: function () {  
+            $(this).parent().parent().css("opacity", 0.2);
+            $(this).parent().parent().css("background-color", "orangered");
+        },
+        mouseleave: function () {  
+            $(this).parent().parent().css("opacity", 1);
+            $(this).parent().parent().css("background-color", "white");
+        },
+        click: function () {  
+            $(this).parent().parent().remove();
+            let x = $(this).parent().parent().children(0).html();
+            let i = localStorage.length;
+            while (i-- > 0) {
+            let key = localStorage.key(i);
+            if (localStorage.getItem(key) === x) {
+                localStorage.removeItem(key);
+                while (Number(key) <= localStorage.length) {
+                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
+                    key++
+                }
+                localStorage.removeItem(localStorage.length);
+            }
+        }
+        }
+    });
 }
 
+$(localStorage).each(function (index) {
+    $(".items").append(newItems);
+    $(`.item:eq(${index})`).html(localStorage.getItem(index+1));
+    deleteItem();
+});
 $("#button2").click(function () { 
-    localStorage.clear()
-    
+    localStorage.clear();
+    $(".newItems").remove();
 });
 $("#button1").click(function () { 
     count = $(".newItems").length;
-
         let item = document.getElementsByClassName("item");
         $(".items").append(newItems);
         localStorage.setItem(item.length, $("#input").val());
         $(`.item:eq(${count})`).html(localStorage.getItem(item.length));
-        console.log(localStorage.length);
-        console.log(localStorage.getItem(1));
-        console.log(localStorage.getItem(localStorage.length));
-        $(".delete").on({
-            mouseover: function () {  
-                $(this).parent().css("opacity", 0.2);
-            },
-            mouseleave: function () {  
-                $(this).parent().css("opacity", 1);
-            }
-        });
+        deleteItem();
     });
 });
