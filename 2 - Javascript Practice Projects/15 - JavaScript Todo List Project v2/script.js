644ed1ba98a32325d3508ca37c2bf76f8698c82e
cptemre <kunduraci2@gmail.com>
2020-10-05 21:40:44 +0300

    Fix 3

--- 2 - Javascript Practice Projects/15 - JavaScript Todo List Project v2/script.js
+++ 2 - Javascript Practice Projects/15 - JavaScript Todo List Project v2/script.js
@@ -25,78 +25,6 @@ function addItem() {
 $(localStorage).each(function (index) {
         $(".items").append(newItems);
         $(`.item:eq(${index})`).html(localStorage.getItem(index+1));
-        (function deleteItem() {
-            $(".deleteIcon").on({
-                mouseover: function () {  
-                    $(this).parent().parent().css("opacity", 0.2);
-                    $(this).parent().parent().css("background-color", "orangered");
-                },
-                mouseleave: function () {  
-                    $(this).parent().parent().css("opacity", 1);
-                    $(this).parent().parent().css("background-color", "white");
-                },
-                click: function () {  
-                    console.log(localStorage.getItem(localStorage.length))
-                    $(this).parent().parent().remove();
-                    let x = $(this).parent().parent().children(0).html();
-                    let i = localStorage.length;
-                    while (i-- > 0) {
-                    let key = localStorage.key(i);
-                    if (localStorage.getItem(key) === x) {
-                        
-                        while (Number(key) < localStorage.length) {
-                            localStorage.setItem(key, localStorage.getItem(Number(key)+1))
-                            key++
-                            localStorage.removeItem(key);
-                        }
-                        
-                    }
-                }
-                }
-            });
-        })();
-});
-(function deleteItem() {
-    $(".deleteIcon").on({
-        mouseover: function () {  
-            $(this).parent().parent().css("opacity", 0.2);
-            $(this).parent().parent().css("background-color", "orangered");
-        },
-        mouseleave: function () {  
-            $(this).parent().parent().css("opacity", 1);
-            $(this).parent().parent().css("background-color", "white");
-        },
-        click: function () {  
-            console.log(localStorage.getItem(localStorage.length))
-            $(this).parent().parent().remove();
-            let x = $(this).parent().parent().children(0).html();
-            let i = localStorage.length;
-            while (i-- > 0) {
-            let key = localStorage.key(i);
-            if (localStorage.getItem(key) === x) {
-                
-                while (Number(key) < localStorage.length) {
-                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
-                    key++
-                    localStorage.removeItem(key);
-                }
-                
-            }
-        }
-        }
-    });
-})();
-$("#button2").click(function () { 
-    localStorage.clear();
-    $(".newItems").remove();
-});
-$("body").keyup(function (e) { 
-    if (e.keyCode === 13) {
-        addItem();
-    }
-});
-$("#button1").click(function () { 
-        addItem();
         $(".deleteIcon").on({
             mouseover: function () {  
                 $(this).parent().parent().css("opacity", 0.2);
@@ -114,12 +42,97 @@ $("#button1").click(function () {
                 while (i-- > 0) {
                 let key = localStorage.key(i);
                 if (localStorage.getItem(key) === x) {
-                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
+                    if (Number(key) == localStorage.length) {
+                        localStorage.setItem(key, localStorage.getItem(Number(key)+1))
+                        localStorage.removeItem(localStorage.length);
+                    }
+                    while (Number(key) < localStorage.length) {
+                        localStorage.setItem(key, localStorage.getItem(Number(key)+1))
                         key++
                         localStorage.removeItem(key);
+                    }
+                    
                 }
             }
             }
-        });    
+        });
+});
+function deleteItem() {
+    $(".deleteIcon").on({
+        mouseover: function () {  
+            $(this).parent().parent().css("opacity", 0.2);
+            $(this).parent().parent().css("background-color", "orangered");
+        },
+        mouseleave: function () {  
+            $(this).parent().parent().css("opacity", 1);
+            $(this).parent().parent().css("background-color", "white");
+        },
+        click: function () {  
+            console.log(localStorage.getItem(localStorage.length))
+            $(this).parent().parent().remove();
+            let x = $(this).parent().parent().children(0).html();
+            let i = localStorage.length;
+            while (i-- > 0) {
+            let key = localStorage.key(i);
+            if (localStorage.getItem(key) === x) {
+                if (Number(key) == localStorage.length) {
+                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
+                    localStorage.removeItem(localStorage.length);
+                }
+                while (Number(key) < localStorage.length) {
+                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
+                    key++
+                    localStorage.removeItem(key);
+                }
+                
+            }
+        }
+        }
+    });
+};
+$("#button2").click(function () { 
+    localStorage.clear();
+    $(".newItems").remove();
+});
+$("body").keyup(function (e) { 
+    if (e.keyCode === 13) {
+        addItem();
+    }
+});
+$("#button1").click(function () { 
+        addItem();
+        (function deleteItem() {
+    $(".deleteIcon").on({
+        mouseover: function () {  
+            $(this).parent().parent().css("opacity", 0.2);
+            $(this).parent().parent().css("background-color", "orangered");
+        },
+        mouseleave: function () {  
+            $(this).parent().parent().css("opacity", 1);
+            $(this).parent().parent().css("background-color", "white");
+        },
+        click: function () {  
+            console.log(localStorage.getItem(localStorage.length))
+            $(this).parent().parent().remove();
+            let x = $(this).parent().parent().children(0).html();
+            let i = localStorage.length;
+            while (i-- > 0) {
+            let key = localStorage.key(i);
+            if (localStorage.getItem(key) === x) {
+                if (Number(key) == localStorage.length) {
+                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
+                    localStorage.removeItem(localStorage.length);
+                }
+                while (Number(key) < localStorage.length) {
+                    localStorage.setItem(key, localStorage.getItem(Number(key)+1))
+                    key++
+                    localStorage.removeItem(key);
+                }
+                
+            }
+        }
+        }
+    });
+})();   
     });
 });
