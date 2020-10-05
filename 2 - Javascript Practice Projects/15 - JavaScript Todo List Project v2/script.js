fe063099c44c1f822d10acd50ad6fc92d1e00ff4
cptemre <kunduraci2@gmail.com>
2020-10-05 22:11:12 +0300

    Fix 4

--- 2 - Javascript Practice Projects/15 - JavaScript Todo List Project v2/script.js
+++ 2 - Javascript Practice Projects/15 - JavaScript Todo List Project v2/script.js
@@ -55,7 +55,6 @@ $(function () {
                             key++
                             localStorage.removeItem(key);
                         }
-
                     }
                 }
             }
