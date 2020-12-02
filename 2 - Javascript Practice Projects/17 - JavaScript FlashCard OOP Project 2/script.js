e0f50e8a4db6ffd7f37cc0e585a8510104cf9f0f
cptemre <kunduraci2@gmail.com>
2020-12-02 17:42:02 +0300

    Final Version
    All functions are created. Visualizing will not change (at least currently).

--- 2 - Javascript Practice Projects/17 - JavaScript FlashCard OOP Project 2/script.js
+++ 2 - Javascript Practice Projects/17 - JavaScript FlashCard OOP Project 2/script.js
@@ -1,6 +1,8 @@
 $(function () {
     var count = 0;
     var myObj = {};
+    var textarea1;
+    var textarea2;
     var cards = `<div class="cards">
     <div class="cardsHeadingDiv">
         <h3 class="cardsHeading">1</h3>
@@ -33,17 +35,17 @@ $(function () {
     }
     console.log(myObj);
     $(".show").on({
-        mouseup: function () {  
+        mouseup: function () {
             $(this).parent().parent().siblings(".cardsAnswerDiv").css("display", "grid").hide().fadeIn(1000);
         }
     });
     $(".hide").on({
-        mouseup: function () {  
+        mouseup: function () {
             $(this).parent().parent().siblings(".cardsAnswerDiv").fadeOut(1000);
         }
     });
     $(".edit").on({
-        mouseup: function () { 
+        mouseup: function () {
             $("#textarea1").val($(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html());
             $("#textarea2").val($(this).parent().siblings(".cardsAnswerDiv").children(".cardsAnswer").html());
             if (count == 0) {
@@ -54,7 +56,7 @@ $(function () {
                 count = 0;
             }
             $("#cancelButton").on({
-                mouseup: function () {  
+                mouseup: function () {
                     $("#mainQuestionDiv").slideUp(1000);
                     count = 0;
                 }
@@ -69,7 +71,7 @@ $(function () {
         }
     });
     $(".delete").on({
-        mouseup: function () {  
+        mouseup: function () {
             $(this).parent().parent().remove();
             var headingName = $(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html();
             console.log(headingName)
@@ -81,7 +83,7 @@ $(function () {
     });
 
     $("#addQuestion").on({
-        mouseup: function () {  
+        mouseup: function () {
             if (count == 0) {
                 $("#mainQuestionDiv").css("display", "grid").hide().slideDown(1000);
                 count = 1;
@@ -90,109 +92,120 @@ $(function () {
                 count = 0;
             }
             $("#cancelButton").on({
-                mouseup: function () {  
+                mouseup: function () {
                     $("#mainQuestionDiv").slideUp(1000);
                     count = 0;
                 }
             });
             $("body").on({
-                keyup: function (e) {  
-                    if (e.keyCode ===13) {
-                        var textarea1 = $("#textarea1").val().trim();
-                        var textarea2 = $("#textarea2").val().trim();
-                        $("#cardsDiv").append(cards);
-                        $(".cardsHeading").last().html(textarea1);
-                        $(".cardsAnswer").last().html(textarea2);
-                        var newObj = {};
-                        myObj[textarea1] = textarea2;
-                        console.log(myObj);
-                        localStorage.setItem('myObj', JSON.stringify(myObj));
-                        console.log(JSON.parse(localStorage.getItem("myObj")));
-                        $(".show").on({
-                            mouseup: function () {  
-                                $(this).parent().parent().siblings(".cardsAnswerDiv").css("display", "grid").hide().fadeIn(1000);
-                            }
-                        });
-                        $(".hide").on({
-                            mouseup: function () {  
-                                $(this).parent().parent().siblings(".cardsAnswerDiv").fadeOut(1000);
-                            }
-                        });
-                        $(".edit").on({
-                            mouseup: function () { 
-                                $("#textarea1").val($(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html());
-                                $("#textarea2").val($(this).parent().siblings(".cardsAnswerDiv").children(".cardsAnswer").html());
-                                $(this).parent().parent().remove();
-                                var headingName = $(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html();
-                                console.log(headingName)
-                                delete myObj[headingName];
-                                console.log(myObj)
-                                localStorage.setItem('myObj', JSON.stringify(myObj));
-                                console.log(JSON.parse(localStorage.getItem("myObj")));
-                            }
-                        });
-                        $(".delete").on({
-                            mouseup: function () {  
-                                $(this).parent().parent().remove();
-                                var headingName = $(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html();
-                                console.log(headingName)
-                                delete myObj[headingName];
-                                console.log(myObj)
-                                localStorage.setItem('myObj', JSON.stringify(myObj));
-                                console.log(JSON.parse(localStorage.getItem("myObj")));
-                            }
-                        });
+                keyup: function (e) {
+                    if (e.keyCode === 13) {
+                        textarea1 = $("#textarea1").val().trim();
+                        textarea2 = $("#textarea2").val().trim();
+                        if (textarea1 in myObj) {
+                            alert(`"${textarea1}" is already in the flash cards.`)
+                        } else {
+                            $("#cardsDiv").append(cards);
+                            $(".cardsHeading").last().html(textarea1);
+                            $(".cardsAnswer").last().html(textarea2);
+                            var newObj = {};
+                            myObj[textarea1] = textarea2;
+                            console.log(myObj);
+                            localStorage.setItem('myObj', JSON.stringify(myObj));
+                            console.log(JSON.parse(localStorage.getItem("myObj")));
+                            $(".show").on({
+                                mouseup: function () {
+                                    $(this).parent().parent().siblings(".cardsAnswerDiv").css("display", "grid").hide().fadeIn(1000);
+                                }
+                            });
+                            $(".hide").on({
+                                mouseup: function () {
+                                    $(this).parent().parent().siblings(".cardsAnswerDiv").fadeOut(1000);
+                                }
+                            });
+                            $(".edit").on({
+                                mouseup: function () {
+                                    $("#textarea1").val($(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html());
+                                    $("#textarea2").val($(this).parent().siblings(".cardsAnswerDiv").children(".cardsAnswer").html());
+                                    $(this).parent().parent().remove();
+                                    var headingName = $(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html();
+                                    console.log(headingName)
+                                    delete myObj[headingName];
+                                    console.log(myObj)
+                                    localStorage.setItem('myObj', JSON.stringify(myObj));
+                                    console.log(JSON.parse(localStorage.getItem("myObj")));
+                                }
+                            });
+                            $(".delete").on({
+                                mouseup: function () {
+                                    $(this).parent().parent().remove();
+                                    var headingName = $(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html();
+                                    console.log(headingName)
+                                    delete myObj[headingName];
+                                    console.log(myObj)
+                                    localStorage.setItem('myObj', JSON.stringify(myObj));
+                                    console.log(JSON.parse(localStorage.getItem("myObj")));
+                                }
+                            });
+                        }
+                    } else if (e.keyCode === 27) {
+                        $("#mainQuestionDiv").slideUp(1000);
+                        count = 0;
                     }
                 }
             })
         }
     });
     $("#saveButton").on({
-        mouseup: function () {  
-            var textarea1 = $("#textarea1").val().trim();
-            var textarea2 = $("#textarea2").val().trim();
-            $("#cardsDiv").append(cards);
-            $(".cardsHeading").last().html(textarea1);
-            $(".cardsAnswer").last().html(textarea2);
-            var newObj = {};
-            myObj[textarea1] = textarea2;
-            console.log(myObj);
-            localStorage.setItem('myObj', JSON.stringify(myObj));
-            console.log(JSON.parse(localStorage.getItem("myObj")));
-            $(".show").on({
-                mouseup: function () {  
-                    $(this).parent().parent().siblings(".cardsAnswerDiv").css("display", "grid").hide().fadeIn(1000);
-                }
-            });
-            $(".hide").on({
-                mouseup: function () {  
-                    $(this).parent().parent().siblings(".cardsAnswerDiv").fadeOut(1000);
-                }
-            });
-            $(".edit").on({
-                mouseup: function () { 
-                    $("#textarea1").val($(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html());
-                    $("#textarea2").val($(this).parent().siblings(".cardsAnswerDiv").children(".cardsAnswer").html());
-                    $(this).parent().parent().remove();
-                    var headingName = $(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html();
-                    console.log(headingName)
-                    delete myObj[headingName];
-                    console.log(myObj)
-                    localStorage.setItem('myObj', JSON.stringify(myObj));
-                    console.log(JSON.parse(localStorage.getItem("myObj")));
-                }
-            });
-            $(".delete").on({
-                mouseup: function () {  
-                    $(this).parent().parent().remove();
-                    var headingName = $(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html();
-                    console.log(headingName)
-                    delete myObj[headingName];
-                    console.log(myObj)
-                    localStorage.setItem('myObj', JSON.stringify(myObj));
-                    console.log(JSON.parse(localStorage.getItem("myObj")));
-                }
-            });
+        mouseup: function () {
+            textarea1 = $("#textarea1").val().trim();
+            textarea2 = $("#textarea2").val().trim();
+            if (textarea1 in myObj) {
+                alert(`"${textarea1}" is already in the flash cards.`)
+            } else {
+                $("#cardsDiv").append(cards);
+                $(".cardsHeading").last().html(textarea1);
+                $(".cardsAnswer").last().html(textarea2);
+                var newObj = {};
+                myObj[textarea1] = textarea2;
+                console.log(myObj);
+                localStorage.setItem('myObj', JSON.stringify(myObj));
+                console.log(JSON.parse(localStorage.getItem("myObj")));
+                $(".show").on({
+                    mouseup: function () {
+                        $(this).parent().parent().siblings(".cardsAnswerDiv").css("display", "grid").hide().fadeIn(1000);
+                    }
+                });
+                $(".hide").on({
+                    mouseup: function () {
+                        $(this).parent().parent().siblings(".cardsAnswerDiv").fadeOut(1000);
+                    }
+                });
+                $(".edit").on({
+                    mouseup: function () {
+                        $("#textarea1").val($(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html());
+                        $("#textarea2").val($(this).parent().siblings(".cardsAnswerDiv").children(".cardsAnswer").html());
+                        $(this).parent().parent().remove();
+                        var headingName = $(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html();
+                        console.log(headingName)
+                        delete myObj[headingName];
+                        console.log(myObj)
+                        localStorage.setItem('myObj', JSON.stringify(myObj));
+                        console.log(JSON.parse(localStorage.getItem("myObj")));
+                    }
+                });
+                $(".delete").on({
+                    mouseup: function () {
+                        $(this).parent().parent().remove();
+                        var headingName = $(this).parent().siblings(".cardsHeadingDiv").children(".cardsHeading").html();
+                        console.log(headingName)
+                        delete myObj[headingName];
+                        console.log(myObj)
+                        localStorage.setItem('myObj', JSON.stringify(myObj));
+                        console.log(JSON.parse(localStorage.getItem("myObj")));
+                    }
+                });
+            }
         }
     });
 });
